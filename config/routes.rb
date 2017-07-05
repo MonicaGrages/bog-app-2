953dfc17771a6eeaf647458e87dc318d8d675ad2
MonicaGrages <mgrages1@gmail.com>
2017-07-05 15:50:18 -0400

    new creature form and create route working

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
 
   get "/creatures", to: "creatures#index", as: "creatures"
   root "creatures#index"
-
+  get "/creatures/new", to: "creatures#new", as: "new_creature"
+  post "creatures", to: "creatures#create"
 end
