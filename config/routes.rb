cc039dc7601a782c5c4aff2ff85a770560e285aa
MonicaGrages <mgrages1@gmail.com>
2017-07-05 14:47:43 -0400

    set up index route and basic view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "/creatures", to: "creatures#index", as: "creatures"
+  root "creatures#index"
+
 end
